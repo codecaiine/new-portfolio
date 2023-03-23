ca1b9dbb998ddcd360955adc19fce42cd76fdb00
codecaiine <akabrouyannickn@gmail.com>
2023-03-23 17:50:34 +0000

    Remove unused package

--- pages/experience.js
+++ pages/experience.js
@@ -1,30 +1,19 @@
 import NextLink from 'next/link'
 import {
-  Link,
   Container,
   Heading,
   Box,
   SimpleGrid,
   Button,
-  List,
-  ListItem,
-  useColorModeValue,
   chakra
 } from '@chakra-ui/react'
 import { ChevronRightIcon, EmailIcon } from '@chakra-ui/icons'
-import Paragraph from '../components/paragraph'
 import { BioSection, BioYear } from '../components/bio'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
-import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
 import thumbYouTube from '../public/images/projects/cottonbro.jpg'
 import thumbInkdrop from '../public/images/projects/cottonbro.jpg'
-import Image from 'next/image'
-
-const ProfileImage = chakra(Image, {
-  shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
-})
 
 const Experience = () => (
   <Layout title="Experience">

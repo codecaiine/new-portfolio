fc8f92165f68d64a561e91fbf1963f88fbfa10a4
codecaiine <akabrouyannickn@gmail.com>
2023-03-28 11:44:46 +0000

    Remove animation

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -10,21 +10,21 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
-        <meta name="author" content="Takuya Matsuyama" />
-        <meta name="author" content="craftzdog" />
+        <meta name="description" content="Yannick's homepage" />
+        <meta name="author" content="Yannick-Noel AKA" />
+        <meta name="author" content="codecaiine" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Takuya Matsuyama" />
+        <meta name="twitter:title" content="Yannick-Noel AKA" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama" />
-        <meta name="og:title" content="Takuya Matsuyama" />
+        <meta name="twitter:site" content="@codecaiine" />
+        <meta name="twitter:creator" content="@codecaiine" />
+        <meta name="twitter:image" content="https://www.github.com/codecaiine" />
+        <meta property="og:site_name" content="Yannick-Noel AKA" />
+        <meta name="og:title" content="Yannick-Noel AKA" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
-        <title>Takuya Matsuyama - Homepage</title>
+        <meta property="og:image" content="https://www.github.com/codecaiine" />
+        <title>Yannick-Noel - Homepage</title>
       </Head>
 
       <NavBar path={router.asPath} />
